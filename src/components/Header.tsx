b7e4fe07ce79ae37cee7af41fb8b6e1d33c2e264
meekkaran <meekkaran@gmail.com>
2024-02-01 15:32:03 +0300

    static user mgmt page and a static landing page

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      {/* Your header content goes here */}
+      <h1>My Common Header</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+// import '../styles/global.css'; 
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Retail Voyager</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
